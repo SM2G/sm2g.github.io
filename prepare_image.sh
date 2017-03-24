b46f5691b9e589fe16817c12ab54427b72714654
SM2G <samy.gejz@gmail.com>
2017-03-24 12:25:02 +0100

    Improved image preparation script.

--- prepare_image.sh
+++ prepare_image.sh
@@ -8,13 +8,15 @@ fi
 image=$1
 keyword=$2
 
+wget -q "${image}" -O ${keyword}.jpg
+
 # Images were designed to be 1024x600 pixels, with teaser images being 1024x380 pixels.
 
 echo "Generating ${keyword}.jpg image..."
-convert ${image} -resize 1024x600^ -gravity center -extent 1024x600 images/${keyword}.jpg
+convert ${keyword}.jpg -resize 1024x600^ -gravity center -extent 1024x600 images/${keyword}.jpg
 
 echo "Generating ${keyword}-teaser.jpg image..."
-convert ${image} -resize 1024x300^ -gravity center -extent 1024x380 images/${keyword}-teaser.jpg
+convert ${keyword}.jpg -resize 1024x300^ -gravity center -extent 1024x380 images/${keyword}-teaser.jpg
 
 echo "Cleanup."
-rm ${image}
+rm ${keyword}.jpg
